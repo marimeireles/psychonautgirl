8432ee2c03fb5d0e74d1ae54cbe945a07a3daea4
mari <marianameireles@protonmail.com>
2025-10-28 13:16:23 +0100

    Auto-commit before deploy

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Dashboard from "./pages/Dashboard";
-import Anki from "./pages/Anki";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -19,7 +18,6 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/anki" element={<Anki />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
--- src/components/StartMenu.tsx
+++ src/components/StartMenu.tsx
@@ -104,14 +104,6 @@ export const StartMenu = ({ isOpen, onClose, onOpenBlog, onOpenWindow }: StartMe
                   onClose();
                 }}
               />
-              <MenuItem
-                icon="🎴"
-                text="Anki Flashcards"
-                onClick={() => {
-                  navigate('/anki');
-                  onClose();
-                }}
-              />
             </div>
           </div>
         </div>
