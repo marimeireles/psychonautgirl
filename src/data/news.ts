386d577c5a239ddd1dcfa4af1b45849ab3d09dae
mari <marianameireles@protonmail.com>
2026-07-10 10:26:48 -0700

    project and news

--- src/data/news.ts
+++ src/data/news.ts
@@ -7,8 +7,9 @@ export interface NewsItem {
 
 const dataString =
 `Title|Date|Summary|Link
+Collective Intelligence Fellow|2026-07-10|Improving democracy at scale by giving disagreement the proper credit|https://www.cip.org/
 Foresight Institute Secure & Sovereign AI Workshop|(Upcoming) 2026-07-12|Speaking on hardening AI systems against prompt injection|
-ICML 2026 Supercooperation: The Future of AI for Democracy |(Upcoming) 2026-07-07|My perspectives on how the structure of disagreement is being dealt with in LLMs|
+ICML 2026 Supercooperation: The Future of AI for Democracy |2026-07-07|My perspectives on how the structure of disagreement is being dealt with in LLMs|
 7th International Conference on the Mathematics of Neuroscience and AI| 2026-06-09|Poster on the development of synergistic cores in LLMs|https://docs.google.com/presentation/d/1EVvv3vadOcvRphoX2rXmXPZ-C6omAOlZbLuYF__XuTU/edit?slide=id.g3a3e10a3c29_0_168#slide=id.g3a3e10a3c29_0_168
 ICLR 2026|2026-04-20|Presented poster on benchmarking LLM web agents|https://scholar.google.com/citations?view_op=view_citation&hl=en&user=oTW1oIEAAAAJ&citation_for_view=oTW1oIEAAAAJ:zYLM7Y9cAGgC
 Cooperative AI Research Fellowship|2026-03-28|Poster on LLM coordination via RLVR through social synchronization|https://docs.google.com/presentation/d/1zWLlQwXBzwCVEY8UFYCmLxaLEG9RzTfwsD_UsSqzpsc/edit?slide=id.g3a3e10a3c29_0_168#slide=id.g3a3e10a3c29_0_168
